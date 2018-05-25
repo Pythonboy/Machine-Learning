a48cc07f3187e552addb1caa5a1f6f57c2326bf6
Pythonboy <37174250+Pythonboy@users.noreply.github.com>
2018-05-25 11:53:58 +0800

    metrics.py
    增加了分类算法的评价标注：
    confusion_matrix ; precision_score ; recall_score

--- metrics.py
+++ metrics.py
@@ -12,3 +12,46 @@ def Mean_Absolute_Error(y_test,y_predict):
 
 def r2_score(y_test,y_predict):
 	return 1-Mean_Squared_Error(y_test,y_predict)/np.var(y_test);
+
+def r2_score(y_test,y_predict):
+	return 1-Mean_Squared_Error(y_test,y_predict)/np.var(y_test);
+
+def accuracy_score(y_test,y_predict):
+	return 1.0*np.sum(y_test==y_predict)/len(y_test);
+	
+def TN(y_true,y_predict):
+	assert len(y_true)==len(y_predict);
+	return np.sum((y_true==0) & (y_predict==0));
+	
+def FN(y_true,y_predict):
+	assert len(y_true)==len(y_predict);
+	return np.sum((y_true==1) & (y_predict==0));
+
+def TP(y_true,y_predict):
+	assert len(y_true)==len(y_predict);
+	return np.sum((y_true==1) & (y_predict==1));
+	
+def FP(y_true,y_predict):
+	assert len(y_true)==len(y_predict);
+	return np.sum((y_true==0) & (y_predict==1));
+	
+def Confusion_Matrix(y_true,y_predict):
+	assert len(y_true)==len(y_predict);
+	return np.array([
+	[TN(y_true,y_predict),FP(y_true,y_predict)],
+	[FN(y_true,y_predict),TP(y_true,y_predict)]
+	])
+	
+def precision_score(y_true,y_predict):
+	assert len(y_true)==len(y_predict);
+	try:
+		return TP(y_true,y_predict)/(FP(y_true,y_predict)+TP(y_true,y_predict));
+	except:
+		return 0;
+		
+def recall_score(y_true,y_predict):
+	assert len(y_true)==len(y_predict);
+	try:
+		return TP(y_true,y_predict)/(FN(y_true,y_predict)+TP(y_true,y_predict));
+	except:
+		return 0;
