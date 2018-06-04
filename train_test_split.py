beeac871de1379bcb54a39efbf98522c9f3a620e
Haoran Lou <37174250+Pythonboy@users.noreply.github.com>
2018-06-04 21:14:44 +0800

    训练集和测试集的划分
    1）增加注释
    2）加入随机种子

--- train_test_split.py
+++ train_test_split.py
@@ -1,15 +1,22 @@
+'''
+作者： 楼浩然
+功能： 将传入的样本随机分割为训练集和测试集
+'''
+
 import numpy as np
 
-def train_test_split(X,Y,ratio = 0.2):
-	assert X.shape[0] == Y.shape[0];
-	assert 0.0<=ratio<=1.0;
-	shuffle_index = np.random.permutation(len(X));
-	test_size = int(len(X)*ratio);
+def train_test_split(X,Y,test_ratio = 0.2,random_state = None):
+	assert X.shape[0] == Y.shape[0];      #保证样本数和结果数相等
+	assert 0.0<= test_ratio <=1.0;        #保证分离训练集和测试集的有效性
+	if random_state:                      #传入随机种子，可以使两次不同的训练、测试集的划分一致
+		np.random.seed(random_state);
+	shuffle_index = np.random.permutation(len(X));      #索引随机化过程
+	test_size = int(len(X)*test_ratio);
 	test_index = shuffle_index[:test_size];
 	train_index = shuffle_index[test_size:];
 	x_train = X[train_index];
 	y_train = Y[train_index];
 	x_test = X[test_index];
 	y_test = Y[test_index];
-	return x_train,y_train,x_test,y_test;
+	return x_train,x_test,y_train,y_test;   
 	
