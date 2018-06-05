b5e1f0dbd25314611fb7eb4a9911bc5131a289f3
Haoran Lou <37174250+Pythonboy@users.noreply.github.com>
2018-06-05 23:30:07 +0800

    机器学习算法准确度评价函数
    增加了一些基本注释

--- metrics.py
+++ metrics.py
@@ -6,18 +6,19 @@
 import numpy as np
 from math import sqrt
 
+'''均方误差（Mwan Squared Error): 用于评价回归算法的准确性'''
 def Mean_Squared_Error(y_test,y_predict):
 	return np.sum((y_predict-y_test)**2)/len(y_test);
-	
+
+'''均方根误差( Root Mean Squared Error) : 用于评价回归算法的准确性'''
 def Root_Mean_Squared_Error(y_test,y_predict):
 	return sqrt(Mean_Squared_Error(y_test,y_predict));
-	
+
+'''平方绝对误差（Mean Absolute Error): 用于评价回归算法的准确性'''
 def Mean_Absolute_Error(y_test,y_predict):
 	return np.sum(np.absolute(y_predict-y_test))/len(y_test);
 
-def r2_score(y_test,y_predict):
-	return 1-Mean_Squared_Error(y_test,y_predict)/np.var(y_test);
-
+'''R Squared ; 评价回归算法最好的函数'''
 def r2_score(y_test,y_predict):
 	return 1-Mean_Squared_Error(y_test,y_predict)/np.var(y_test);
 
